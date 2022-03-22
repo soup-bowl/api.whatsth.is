04efb75852ec4e4727a2292402c5f097038846e8
soup-bowl <code@soupbowl.io>
2022-03-22 21:12:38 +0000

    WP object change.

--- api/technology/wordpress.py
+++ api/technology/wordpress.py
@@ -24,50 +24,37 @@ class WordPressIdentifier(object):
 		self.reply.tagline  = self.response['description']
 		self.reply.timezone = self.response['timezone_string']
 
-		#if self.supports_v2:
-		#	reply['content']    = self.page_stats()
-		#	reply['categories'] = self.category_stats()
+		if self.supports_v2:
+			self.page_stats()
+			self.category_stats()
 
 		return self.reply
 
 	def page_stats(self):
 		response_posts = self.get_from_api(self.url + '/wp/v2/posts')
 		response_pages = self.get_from_api(self.url + '/wp/v2/pages')
-		reply = {}
 
 		if response_posts is not None:
-			data = {
-				'count': int(response_posts['headers']['X-WP-Total']),
-			}
+			self.reply.post_count = int(response_posts['headers']['X-WP-Total'])
 
 			if len(response_posts['api'][0]) > 0:
-				data['latest'] = {
-					'title': response_posts['api'][0]['title']['rendered'],
-					'date':  response_posts['api'][0]['date'],
-					'url':   response_posts['api'][0]['link']
-				}
-			else:
-				data['latest'] = False
+				post = Post()
+				post.title = response_posts['api'][0]['title']['rendered']
+				post.date  = response_posts['api'][0]['date']
+				post.url   = response_posts['api'][0]['link']
 
-			reply['posts'] = data
+				self.reply.latest_post = post
 
 		if response_pages is not None:
-			data = {
-				'count': int(response_pages['headers']['X-WP-Total']),
-			}
+			self.reply.post_count = int(response_pages['headers']['X-WP-Total'])
 
 			if len(response_pages['api'][0]) > 0:
-				data['latest'] = {
-					'title': response_pages['api'][0]['title']['rendered'],
-					'date':  response_pages['api'][0]['date'],
-					'url':   response_pages['api'][0]['link']
-				}
-			else:
-				data['latest'] = False
+				page = Post()
+				page.title = response_pages['api'][0]['title']['rendered']
+				page.date  = response_pages['api'][0]['date']
+				page.url   = response_pages['api'][0]['link']
 
-			reply['pages'] = data
-
-		return reply
+				self.reply.latest_page = page
 
 	def category_stats(self):
 		response = self.get_from_api(self.url + '/wp/v2/categories')
@@ -96,13 +83,15 @@ class WordPressIdentifier(object):
 
 class WordPress(object):
 	def __init__(self):
-		self._success    = False
-		self._name       = ''
-		self._tagline    = ''
-		self._timezone   = ''
-		self._post_count = -1
-		self._page_count = -1
-		self._cat_count  = -1
+		self._success     = False
+		self._name        = ''
+		self._tagline     = ''
+		self._timezone    = ''
+		self._post_count  = -1
+		self._page_count  = -1
+		self._cat_count   = -1
+		self._latest_post = None
+		self._latest_page = None
 
 	@property
 	def success(self) -> bool:
@@ -132,6 +121,14 @@ class WordPress(object):
 	def cat_count(self) -> int:
 		return self._cat_count
 
+	@property
+	def latest_post(self):
+		return self._latest_post
+
+	@property
+	def latest_page(self):
+		return self._latest_page
+
 	@success.setter
 	def success(self, state):
 		self._success = state
@@ -160,6 +157,14 @@ class WordPress(object):
 	def cat_count(self, count):
 		self._cat_count = count
 
+	@latest_post.setter
+	def latest_post(self, post):
+		self._latest_post = post
+
+	@latest_page.setter
+	def latest_page(self, page):
+		self._latest_page = page
+
 	def asdict(self):
 		return {
 			'success': self.success,
@@ -169,4 +174,43 @@ class WordPress(object):
 			'post_count': self.post_count,
 			'page_count': self.page_count,
 			'cat_count': self.cat_count,
+			'latest_post': self.latest_post.asdict() if self.latest_post is not None else None,
+			'latest_page': self.latest_page.asdict() if self.latest_page is not None else None,
+		}
+
+class Post(object):
+	def __init__(self):
+		self._title = ''
+		self._date  = ''
+		self._url   = ''
+
+	@property
+	def title(self) -> str:
+		return self._title
+
+	@property
+	def date(self) -> str:
+		return self._date
+
+	@property
+	def url(self) -> str:
+		return self._url
+
+	@title.setter
+	def title(self, title):
+		self._title = title
+
+	@date.setter
+	def date(self, date):
+		self._date = date
+
+	@url.setter
+	def url(self, url):
+		self._url = url
+
+	def asdict(self):
+		return {
+			'title': self.title,
+			'date': self.date,
+			'url': self.url,
 		}
