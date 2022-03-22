20d7231f619a6e19d337fdc17237abe56d966560
soup-bowl <code@soupbowl.io>
2022-03-22 20:55:33 +0000

    Condensed asdict for inpsectionl

--- api/inspection.py
+++ api/inspection.py
@@ -138,60 +138,55 @@ class InspectionResult(object):
 		self._match_count = 0
 		self._match_total = 0
 		self._additional  = None
-	
+
 	@property
 	def technology(self) -> str:
 		return self._technology
-	
+
 	@property
 	def matched_on(self) -> list:
 		return self._matched_on
-	
+
 	@property
 	def match_count(self) -> int:
 		return self._match_count
-	
+
 	@property
 	def match_total(self) -> int:
 		return self._match_total
-	
+
 	@property
 	def additional(self):
 		return self._additional
-	
+
 	@technology.setter
 	def technology(self, technology: str):
 		self._technology = technology
-	
+
 	@matched_on.setter
 	def matched_on(self, matchedon: list):
 		self._matched_on = matchedon
-	
+
 	@match_count.setter
 	def match_count(self, count: int):
 		self._match_count = count
-	
+
 	@match_total.setter
 	def match_total(self, count: int):
 		self._match_total = count
-	
+
 	@additional.setter
 	def additional(self, additional):
 		self._additional = additional
-	
+
 	def add_match(self, match_string):
 		self._matched_on.append(match_string)
 		self._match_count = self._match_count + 1
 		return self
-	
+
 	def asdict(self):
-		obj = {
+		return {
 			'technology': self.technology,
 			'matched_on': self.matched_on,
-			'additional': self.additional,
+			'additional': self.additional.asdict() if self.additional is not None else None,
 		}
-
-		if self.additional is not None:
-			obj['additional'] = self.additional.asdict()
-		
-		return obj
