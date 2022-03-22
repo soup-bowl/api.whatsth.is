33adb91259a320c6e7059c55882832be722477b7
soup-bowl <code@soupbowl.io>
2022-03-22 22:29:58 +0000

    Amended test for changes.

--- tests/test_api.py
+++ tests/test_api.py
@@ -7,17 +7,19 @@ class StatChecks(unittest.TestCase):
 	@classmethod
 	def setUpClass(self):
 		self.config = Config()
-		self.config.load('detection.json')
+		self.config.load_json(
+			'{"cms":{"wordpress":{"headers":["x-powered-by: WP Engine"],"body":["/html/head/link[@href=\'//s.w.org\']"]}}}'
+		)
 
 	def test_generic_detections(self):
-		"""Checks the operationality of the checks.
+		"""Checks the definitions of the checks.
 		"""
 		# WordPress
 		inspector = Inspection(self.config, 'https://wordpress.org/').get_site_details()
-		self.assertEqual(inspector['technology'], 'WordPress')
+		self.assertEqual(inspector.technology, 'WordPress')
 		# Unknown, but exists.
 		inspector = Inspection(self.config, 'https://example.com/').get_site_details()
-		self.assertEqual(inspector['technology'], 'Unknown')
+		self.assertEqual(inspector.technology, 'Unknown')
 
 	def test_nicename(self):
 		"""Check the nice name generator uses the in-built values, and capitalises unknown ones.
