67c7da9f6ba2c1507fbc220b64663e9d3f6bacbe
soup-bowl <code@soupbowl.io>
2022-04-02 16:33:20 +0100

    Added API metadata.

--- api/main.py
+++ api/main.py
@@ -19,7 +19,20 @@ else:
     print("Unable to download definitions file.")
     exit(1)
 
-app = FastAPI()
+app = FastAPI(
+    title="What's This? API",
+    description="Inspection application that detects web technologies and informs the user of them.",
+    contact={
+        "name": "Soupbowl",
+        "email": "code@soupbowl.io",
+        "url": "https://www.soupbowl.io",
+    },
+    license_info={
+        "name": "MIT",
+        "url": "https://github.com/soup-bowl/api.whatsth.is/blob/main/LICENSE",
+    },
+    version="0.2"
+)
 
 app.add_middleware(
     CORSMiddleware,
